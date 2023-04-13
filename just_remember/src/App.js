d205ea75213ac1067c9dc9cb0dcceeca6dddec8d
Dzecki <oliwier.kostrzewski@wp.pl>
2023-04-13 14:02:34 +0200

    nearly an end/ fix id and do check for tasks

--- just_remember/src/App.js
+++ just_remember/src/App.js
@@ -2,9 +2,8 @@ import React, { useState } from 'react'
 import Task from './Components/Task';
 
 export default function App() {
-  let i = 0;
   const [tasks, setTasks] = useState([]);
-  const [task, setTask] = useState({taskID: i, taskTitle: '', taskDescription: '', taskStartDate: '', taskEndDate: ''});
+  const [task, setTask] = useState({taskID: 0, taskTitle: '', taskDescription: '', taskStartDate: '', taskEndDate: ''});
 
   const changeTab = (e) => {
     return e.target.style = "opacity: 100%";
@@ -22,14 +21,15 @@ export default function App() {
     setTasks(remainingTasks);
   } 
 
-  const taskList = tasks.map((task) => (
-    <Task id={task.taskID} title={task.taskTitle} description={task.taskDescription} deleteTask={deleteTask}/>
-  ));
-
   const addTask = () => {
+    console.log(tasks);
+    setTask({taskID: tasks.length});
     setTasks([...tasks, task]);
-    setTask({taskID: 2});
   }
+
+  const taskList = tasks.map((task) => (
+    <Task id={task.taskID} title={task.taskTitle} description={task.taskDescription} startDate={task.taskStartDate} endDate={task.taskEndDate} deleteTask={deleteTask}/>
+  ));
   
   return (
     <div className='flex flex-col items-center justify-center w-[100vw] h-[100vh] bg-gray-700'>
@@ -39,22 +39,28 @@ export default function App() {
           <div className='w-[33%] text-center p-6 cursor-pointer opacity-[80%]' name="todo">TO-DO</div>
           <div className='w-[33%] text-center p-6 cursor-pointer opacity-[80%]' name="done">DONE</div>
         </div>
-        <div className='flex justify-center items-center bg-gray-800 w-[60%] rounded-t-md h-[90px] text-white text-2xl border-2 border-gray-900 border-b-0'>
-          <div className='flex justify-center items-center h-[100%] bg-gray-700 w-[100%] rounded-sm pr-2 text-white text-[18px]'>
-            <div className='flex flex-col w-[40%]'>
-              <input className='m-1 h-[32px] bg-transparent border-b-2 outline-none border-gray-800 p-1' placeholder='title' name='taskTitle' value={task.taskTitle} onChange={handleChange}/>
-              <input className='m-1 h-[32px] bg-transparent border-b-2 outline-none border-gray-800 p-1' placeholder='description' name='taskDescription' value={task.taskDescription} onChange={handleChange}/>
+        <div className='flex justify-around items-center bg-gray-800 w-[60%] rounded-t-md h-[90px] text-white text-2xl border-2 border-gray-900 border-b-0'>
+          <div className='flex justify-center items-center h-[100%] bg-gray-800 w-[100%] rounded-sm text-white text-[18px]'>
+            <div className='flex flex-col w-[60%]'>
+              <input className='m-1 h-[32px] bg-transparent rounded-br-md border-r border-b border-gray-900 p-1' placeholder='title' name='taskTitle' value={task.taskTitle} onChange={handleChange}/>
+              <input className='m-1 h-[32px] bg-transparent rounded-tr-md border-r border-t border-gray-900 p-1' placeholder='description' name='taskDescription' value={task.taskDescription} onChange={handleChange}/>
             </div>
-            <div className='flex flex-col ml-4'>
-              <input className='m-1 h-[33px] bg-transparent border-b-2 border-gray-800 rounded-sm p-1' type="date"></input>
-              <input className='m-1 h-[33px] bg-transparent border-b-2 border-gray-800 rounded-sm p-1' type="date"></input>
+            <div className='flex flex-col ml-1 w-[30%]'>
+              <div className='flex border-l border-b border-gray-900 m-1 h-[32px] rounded-bl-md'>
+                <label className='w-[50px] text-[10px] font-semibold text-center'>START</label>
+                <input className='m-1 bg-transparent border-b-2 border-gray-800 rounded-sm p-1' type="date" name='taskStartDate' value={task.taskStartDate} onChange={handleChange}></input>
+              </div>
+              <div className='flex border-l border-t border-gray-900 m-1 h-[32px] rounded-tl-md'>
+                <label className='w-[50px] text-[10px] font-semibold text-center'>END</label>
+                <input className='m-1 bg-transparent p-1' type="date" name='taskEndDate' value={task.taskEndDate} onChange={handleChange}></input>
+              </div>
             </div>
-              <button className='ml-8 h-[100%] w-[80px] bg-gray-800 ml-2 font-semibold border-4 rounded-xl border-gray-700 hover:bg-gray-600 transition-all' onClick={addTask}>add</button>
-            </div>
+            <button className='h-[100%] w-[80px] bg-gray-900 font-semibold hover:bg-gray-600 transition-all ml-1' onClick={addTask}>add</button>
           </div>
+        </div>
       </div>
 
-      <div className='relative w-[75%] h-[85%] bg-gray-800 rounded-md shadow-sm rounded-r-none rounded-b-md border-2 border-gray-900'>
+      <div className='relative w-[75%] h-[85%] bg-gray-800 rounded-md shadow-sm rounded-r-none rounded-b-md border-2 border-gray-900 overflow-y-scroll scrollbar-hide'>
         <div className='relative flex flex-col justify-center items-center mt-8'>
           {taskList}
         </div>
--- just_remember/src/Components/Task.js
+++ just_remember/src/Components/Task.js
@@ -1,14 +1,21 @@
 import React from 'react'
 
-function Task({id, title, description, startDate, endDate, deleteTask}) {
+function Task(props) {
   return (
-    <div className='flex flex-col items-center w-[45%] h-[150px] my-4 text-gray-1000 border-2 border-gray-900'>
-        <div className='h-[60%] w-[100%]'>
-          <input type="checkbox" className='w-[40px] h-[40px] m-4'></input>
+    <div className='flex flex-col items-center w-[60%] h-[200px] my-4 text-gray-1000 border-2 border-gray-900'>
+        <div className='relative flex flex-col items-center justify-center h-[70%] w-[100%]'>
+          <input type="checkbox" className='absolute top-2 left-2 w-[30px] h-[30px]'></input>
+          <div className='flex justify-between w-[80%] h-[60px] m-4'>
+            <div className='border-2 border-gray-900 w-[40%] text-center text-white font-semibold p-3'>{props.title}{props.id}</div>
+            <div className='border-2 border-gray-900 w-[25%] text-center text-white font-semibold p-3'>{props.startDate}</div>
+            <h1 className='p-2 font-bold text-gray-900 text-xl'>{">"}</h1>
+            <div className='border-2 border-gray-900 w-[25%] text-center text-white font-semibold p-3'>{props.endDate}</div>
+          </div>
+          <div className='border-2 border-gray-900 w-[80%] h-[55px] text-center text-white font-semibold p-3'>{props.description}</div>
         </div>
-        <div className='flex justify-center h-[40%] w-[100%] font-semibold'>
-          <button className='w-[30%] h-[35px] border-2 border-gray-900 m-2 bg-red-400' onClick={() => deleteTask(id)}>DELETE</button>
-          <button className='w-[30%] h-[35px] border-2 border-gray-900 m-2 bg-gray-600'>EDIT</button>
+        <div className='flex justify-center items-end h-[30%] w-[100%] font-semibold'>
+          <button className='w-[30%] h-[35px] border-2 border-gray-900 m-2 bg-red-400' onClick={() => props.deleteTask(props.id)}>DELETE</button>
+          <button className='w-[30%] h-[35px] border-2 border-gray-900 m-2 bg-gray-700'>EDIT</button>
         </div>
     </div>
   )
